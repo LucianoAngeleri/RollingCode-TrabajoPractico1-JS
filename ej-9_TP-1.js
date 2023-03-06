0b07095fb15bc0ccee582a8f179a1fe798c0bdf2
Luciano Angeleri <lcnang@gmail.com>
2023-03-06 11:15:22 -0300

    Se modificó la carpeta del ejercicio 9 y se agregó la funcionalidad para encontrar las vocales

--- ej-9_TP-1.js
+++
@@ -1,5 +0,0 @@
-/*
-Escribe un programa que pida una frase y escriba las vocales que aparecen. 
-Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
-*/
-let frase = prompt("Ingrese una frase: ");
