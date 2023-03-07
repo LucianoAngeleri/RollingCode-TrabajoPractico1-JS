d743b0ddc549f468c03c37f7b14ccaac18fdcead
Luciano Angeleri <lcnang@gmail.com>
2023-03-07 05:20:22 -0300

    Se añadió el ejercicio 10 y su resolución

---
+++ js/ej-10_TP-1.js
@@ -0,0 +1,20 @@
+/*
+Ejercicio 10
+Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 
+(sólo hay que comprobar si lo es por uno de los cuatro)
+*/
+let numero = prompt("Ingrese un número: ");
+let esDivisiblePor = "";
+
+if (numero % 2 === 0) {
+    esDivisiblePor = `El número ${numero} es divisible por 2`;
+}else if(numero % 3 === 0){
+    esDivisiblePor = `El número ${numero} es divisible por 3`;
+}else if(numero % 5 === 0){
+    esDivisiblePor = `El número ${numero} es divisible por 5`;
+}else if(numero % 7 === 0){
+    esDivisiblePor = `El número ${numero} es divisible por 7`;
+}else{
+    esDivisiblePor=`El número ${numero} NO es divisible ni por 2, 3, 5 o 7`
+}
+document.write(`<h1>${esDivisiblePor}</h1>`);
