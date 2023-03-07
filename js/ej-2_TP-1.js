c453ffefe928c59c381939d24bc44387235f4876
Luciano Angeleri <lcnang@gmail.com>
2023-03-07 15:42:49 -0300

    Se añadió la página que muestra el ejercicio 3 y los anteriores

--- js/ej-2_TP-1.js
+++ js/ej-2_TP-1.js
@@ -2,4 +2,4 @@
 Escribe un programa de una sola línea 
 que escriba en la pantalla un texto que diga «Hello World» (document.write).
 */
-document.write("<h1>Hello World</h1>")
+document.write("<p class='fs-3 text-center rounded-pill text-bg-secondary p-3'>Hello World</p>")
--- js/ej-3_TP-1.js
+++ js/ej-3_TP-1.js
@@ -4,4 +4,4 @@ Ejercicio 3
 Escribe un programa de una sola línea 
 que escriba en la pantalla el resultado de sumar 3 + 5.
 */
-document.write(`<h1>El resultado de sumar 3 + 5 es ${3 + 5}</h1>`);
+document.write(`<p class="fs-3 text-center rounded-pill text-bg-secondary p-3">El resultado de sumar 3 + 5 es ${3 + 5}</p>`);
