7c4f43b9735cc881cc5532e76431b341749a4ab1
Luciano Angeleri <lcnang@gmail.com>
2023-03-07 06:21:35 -0300

    Se corrigío un error de tipeo

--- js/ej-3_TP-1.js
+++ js/ej-3_TP-1.js
@@ -4,4 +4,4 @@ Ejercicio 3
 Escribe un programa de una sola línea 
 que escriba en la pantalla el resultado de sumar 3 + 5.
 */
-document.write(`<h1>El resultado de sumas 3 + 5 es ${3 + 5}</h1>`);
+document.write(`<h1>El resultado de sumar 3 + 5 es ${3 + 5}</h1>`);
