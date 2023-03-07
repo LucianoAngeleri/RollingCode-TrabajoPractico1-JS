26487314c421f3416dd85486442d4e8608563c34
Luciano Angeleri <lcnang@gmail.com>
2023-03-07 15:51:01 -0300

    Se añadió la página que muestra el ejercicio 7

--- js/ej-7_TP-1.js
+++ js/ej-7_TP-1.js
@@ -7,9 +7,9 @@ let number1 = parseInt(prompt("Ingrese un número: "));
 let number2 = parseInt(prompt("Ingrese otro número: "));
 let number3 = parseInt(prompt("Ingrese otro número: "));
 if (number1 > number2 && number1 > number3) {
-    document.write(`<h1>El numero ${number1} es mayor que los números ${number2} y ${number3}</h1>`);
+    document.write(`<p class="fs-3 text-center rounded-pill text-bg-secondary p-3">El numero ${number1} es mayor que los números ${number2} y ${number3}</p>`);
 }else if(number2 > number1 && number2 > number3){    
-    document.write(`<h1>El numero ${number2} es mayor que los números ${number1} y ${number3}</h1>`);
+    document.write(`<p class="fs-3 text-center rounded-pill text-bg-secondary p-3">El numero ${number2} es mayor que los números ${number1} y ${number3}</p>`);
 }else if(number3 > number1 && number3 > number1){    
-    document.write(`<h1>El numero ${number3} es mayor que los números ${number1} y ${number2}</h1>`);
+    document.write(`<p class="fs-3 text-center rounded-pill text-bg-secondary p-3">El numero ${number3} es mayor que los números ${number1} y ${number2}</p>`);
 }
